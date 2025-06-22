e8f1e4de57a2fb698e372fac454deed2530b5aa1
Prateem <jyotiprateem05@gmail.com>
2025-06-22 15:28:05 +0530

    Changes in env file for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    baseUrl: "https://ings-backend.onrender.com",
+    redirectURI: "https://ings-frontend.onrender.com",
+    clientId: '6ef37588-c506-4766-94dc-3f6bdd81f09e',
+    authority: 'https://login.microsoftonline.com/881bcb6a-2699-432e-98b6-62baad963ca4',
+}
